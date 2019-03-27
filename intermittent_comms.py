e9adc76c7d295f93cd2568bfac61ae7e5463ff73
Tahiya Salam <tahiyasalam@gmail.com>
2019-03-27 13:12:19 -0400

    testing schedule making

--- intermittent_comms.py
+++ intermittent_comms.py
@@ -1,13 +1,41 @@
 import numpy as np
 
 
+def find_nearest_above(my_array, target):
+    diff = my_array - target
+    mask = np.ma.less_equal(diff, 0)
+    # We need to mask the negative differences and zero
+    # since we are looking for values above
+    if np.all(mask):
+        return None # returns None if target is greater than any value
+    masked_diff = np.ma.masked_array(diff, mask)
+    return masked_diff.argmin()
+
+
+# TODO: double-check to make sure this works
+def find_nearest_below(my_array, target):
+    diff = my_array - target
+    mask = np.ma.greater_equal(diff, 0)
+    # We need to mask the negative differences and zero
+    # since we are looking for values above
+    if np.all(mask):
+        return None # returns None if target is greater than any value
+    masked_diff = np.ma.masked_array(diff, mask)
+    return masked_diff.argmax()
+
+
 class Robot:
+    objs = []  # Registrar
+    discretization = (600, 600)
+
     def __init__(self, ID, teams, schedule):
         self.ID = ID
         self.teams = teams
         self.schedule = schedule
-        self.sensor_data = {}
+        self.active_locations = {} # Store active location as indexed by (start_time, end_time): locations
+        self.sensor_data = {} # Store data as (start_time, end_time): data
         self.eigen_data = {}
+        Robot.objs.append(self)
 
     def add_new_data(self, new_data, time, data_type):
         t_start, t_end = time
@@ -22,14 +50,31 @@ class Robot:
         else:
             self.eigen_data = {**self.eigen_data, **new_data}
 
-    def construct_data_matrix(self):
+    @classmethod
+    def construct_data_matrix(cls):
         from operator import itemgetter
-
+        max_t = 0
         # Find end time of data matrix
-        keys = self.sensor_data.keys()
-        max_t = max(keys, key=itemgetter(2))[0]  # Returns largest end time
-        data_matrix = np.array()
-        return None
+        for obj in cls.objs:
+            keys = obj.sensor_data.keys()
+            max_from_keys = max(keys, key=itemgetter(2))[0]  # Returns largest end time
+            if max_t < max_from_keys:
+                max_t = max_from_keys
+
+        data_matrix = np.zeros((Robot.discretization[0], Robot.discretization[1], max_t))
+
+        for obj in cls.objs:  # Fill in data matrix
+            active_location_times = obj.active_locations.keys()
+            active_location_start = active_location_times[0]
+            active_location_end = active_location_times[1]
+
+            # Match sensor data start and end time to active locations
+            for key, data in obj.sensor_data:
+                data_start_t, data_end_t = key[0], key[1]
+
+                data_matrix[obj.active_locations[0], obj.active_locations[1], data_start_t:data_end_t] = data
+
+        return data_matrix
 
 
 class Schedule:
@@ -88,8 +133,3 @@ class Schedule:
 
         schedule = schedule[:, ~np.all(schedule == 0, axis=0)]  # Remove columns full of zeros
         return schedule
-
-
-
-
-
